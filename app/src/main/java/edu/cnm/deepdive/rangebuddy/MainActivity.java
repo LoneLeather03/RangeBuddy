7b351f41bf4f58d445fc197518c37bddb2fe7192
LoneLeather03 <davidmartinez333@gmail.com>
2017-08-03 16:52:15 -0600

    added on touch listener to clickable Compass image and math to use touch on the Compass image to get direction and wind speed.

--- app/src/main/java/edu/cnm/deepdive/rangebuddy/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/rangebuddy/MainActivity.java
@@ -8,8 +8,10 @@ import android.graphics.drawable.shapes.OvalShape;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.j256.ormlite.dao.Dao;
 import com.j256.ormlite.stmt.QueryBuilder;
@@ -25,6 +27,8 @@ import edu.cnm.deepdive.rangebuddy.views.CustomDrawableView;
 import edu.cnm.deepdive.rangebuddy.views.TestActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int direction = 0;
 
 
     @Override
@@ -43,12 +47,41 @@ public class MainActivity extends AppCompatActivity {
         });
 
         Button listButton = (Button) findViewById(R.id.listButton);
-        listButton.setOnClickListener(new View.OnClickListener(){
-        public void onClick(View v) {
+        listButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
                 Intent listActivity = new Intent(MainActivity.this, TestActivity.class);
                 startActivity(listActivity);
-        }
-    });
+            }
+        });
+
+//        Button nW = (Button) findViewById(R.id.northwest);
+//        nW.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                direction = 315;
+//            }
+//        });
+        ImageView iv = (ImageView) findViewById(R.id.Compass);
+        iv.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int height = v.getHeight();
+                int width = v.getWidth();
+                float xCenter = width / 2.0f;
+                float yCenter = height / 2.0f;
+                float deltaX = event.getX() - xCenter;
+                float deltaY = event.getY() - yCenter;
+                double r = Math.hypot(deltaX, deltaY);
+                double speed = 60 * Math.min(2 * r / width, 1);
+                double angle = 180 / Math.PI * Math.atan2(deltaX, -deltaY);
+
+                Log.d("Compass click", "Location: " + event.getX() + " , " + event.getY());
+                Log.d("Compass click", String.format("Vector: (%f, %f)%n", speed, angle));
+
+                return true;
+            }
+        });
 
     }
+
 }
